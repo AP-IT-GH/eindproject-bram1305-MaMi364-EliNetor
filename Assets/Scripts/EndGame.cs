3a3e9f94e511560192fd4208001218c36dbcc4ea
Eli Vermeulen <eli.vermeulen2002@gmail.com>
2024-06-15 17:28:27 +0200

    Add placements and ending screen

--- Assets/Scripts/EndGame.cs
+++ Assets/Scripts/EndGame.cs
@@ -7,47 +7,70 @@ using UnityEngine.XR;
 
 public class EndGame : MonoBehaviour
 {
-    public GameObject agent;
+    public GameObject[] agent;
     public GameObject player;
     public TextMeshProUGUI endText;
     public GameObject EndingScreen;
     private Rigidbody rbPlayer;
-    private Rigidbody rbAgent;
+    private Rigidbody[] rbAgent;
     private bool end = false;
-    
+
+    private void Start()
+    {
+        // Initialize the rbAgent array with the correct size
+        rbAgent = new Rigidbody[agent.Length];
+
+        // Assign Rigidbody components to each agent's Rigidbody in the rbAgent array
+        for (int i = 0; i < agent.Length; i++)
+        {
+            rbAgent[i] = agent[i].GetComponent<Rigidbody>();
+        }
+
+        // Assign Rigidbody component to the player's Rigidbody
+        rbPlayer = player.GetComponent<Rigidbody>();
+    }
+
     private void Update()
     {
         if (end)
         {
+            // Stop player's and agents' movement and gravity
             rbPlayer.velocity = Vector3.zero;
             rbPlayer.useGravity = false;
-            rbAgent.velocity = Vector3.zero;
-            rbAgent.useGravity = false;
+
+            foreach (Rigidbody agentRB in rbAgent)
+            {
+                agentRB.velocity = Vector3.zero;
+                agentRB.useGravity = false;
+            }
         }
     }
-    private void Start()
-    {
-        rbPlayer = player.GetComponent<Rigidbody>();
-        rbAgent = player.GetComponent<Rigidbody>();
-    }
+
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject == agent && rbAgent.velocity.y == 0)
+        // Check collisions with each agent and the player
+        for (int i = 0; i < agent.Length; i++)
         {
-            endText.text = "You lose";
-            EndingScreenEnable();
+            if (collision.gameObject == agent[i] && rbAgent[i].velocity.y == 0)
+            {
+                endText.text = "You lose";
+                EndingScreenEnable();
+                return; // Exit early once we find a matching agent
+            }
         }
-        else if (collision.gameObject == player && rbPlayer.velocity.y == 0) 
+
+        if (collision.gameObject == player && rbPlayer.velocity.y == 0)
         {
             endText.text = "You win";
             EndingScreenEnable();
         }
     }
+
     private void EndingScreenEnable()
-    {      
+    {
         end = true;
         EndingScreen.SetActive(true);
         Vector3 playerPosition = player.transform.position;
-        EndingScreen.transform.position = new Vector3(playerPosition.x, playerPosition.y, playerPosition.z + 20f);
+        EndingScreen.transform.position = new Vector3(playerPosition.x, playerPosition.y, playerPosition.z + 5f);
     }
 }
--- Assets/Scripts/Placement.cs
+++ Assets/Scripts/Placement.cs
@@ -1,32 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
+using TMPro;
 
 public class Placement : MonoBehaviour
 {
-    // Public variables to assign the two objects and the TextMeshPro component in the Inspector
+    // Public variables to assign the five objects and the TextMeshPro component in the Inspector
     public GameObject object1;
     public GameObject object2;
+    public GameObject object3;
+    public GameObject object4;
+    public GameObject object5;
     public TextMeshProUGUI positionText;
 
     void Update()
     {
-        // Check if the objects are assigned
-        if (object1 != null && object2 != null)
+        // Check if all objects are assigned
+        if (object1 != null && object2 != null && object3 != null && object4 != null && object5 != null)
         {
-            // Calculate the distances from the finish line
+            // Calculate the distances from the current object to each of the five objects
             float distance1 = Vector3.Distance(transform.position, object1.transform.position);
             float distance2 = Vector3.Distance(transform.position, object2.transform.position);
+            float distance3 = Vector3.Distance(transform.position, object3.transform.position);
+            float distance4 = Vector3.Distance(transform.position, object4.transform.position);
+            float distance5 = Vector3.Distance(transform.position, object5.transform.position);
 
-            // Update the TextMeshPro component based on which object is closer
-            if (distance1 < distance2)
+            // Determine the position of object1 relative to the others
+            int position = 1; // Start with 1st position as default
+
+            if (distance1 > distance2) position++;
+            if (distance1 > distance3) position++;
+            if (distance1 > distance4) position++;
+            if (distance1 > distance5) position++;
+
+            // Update the TextMeshPro component based on the position of object1
+            switch (position)
             {
-                positionText.text = "1st";
-            }
-            else
-            {
-                positionText.text = "2nd";
+                case 1:
+                    positionText.text = "1st";
+                    break;
+                case 2:
+                    positionText.text = "2nd";
+                    break;
+                case 3:
+                    positionText.text = "3rd";
+                    break;
+                case 4:
+                    positionText.text = "4th";
+                    break;
+                case 5:
+                    positionText.text = "5th";
+                    break;
+                default:
+                    positionText.text = "Unknown";
+                    break;
             }
         }
     }
