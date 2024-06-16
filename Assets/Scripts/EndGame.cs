a3411f5a5225070fcf97a9ef750520f5a7c11dd2
Eli Vermeulen <eli.vermeulen2002@gmail.com>
2024-06-16 14:09:19 +0200

    Add win and lose scene

--- Assets/Scripts/EndGame.cs
+++ Assets/Scripts/EndGame.cs
@@ -4,17 +4,14 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.XR;
+using UnityEngine.SceneManagement;
 
 public class EndGame : MonoBehaviour
 {
     public GameObject[] agent;
     public GameObject player;
-    public TextMeshProUGUI endText;
-    public GameObject EndingScreen;
-    public GameObject EndSound;
     private Rigidbody rbPlayer;
     private Rigidbody[] rbAgent;
-    private bool end = false;
 
     private void Start()
     {
@@ -31,22 +28,6 @@ public class EndGame : MonoBehaviour
         rbPlayer = player.GetComponent<Rigidbody>();
     }
 
-    private void Update()
-    {
-        if (end)
-        {
-            // Stop player's and agents' movement and gravity
-            rbPlayer.velocity = Vector3.zero;
-            rbPlayer.useGravity = false;
-
-            foreach (Rigidbody agentRB in rbAgent)
-            {
-                agentRB.velocity = Vector3.zero;
-                agentRB.useGravity = false;
-            }
-        }
-    }
-
     private void OnCollisionEnter(Collision collision)
     {
         // Check collisions with each agent and the player
@@ -54,25 +35,19 @@ public class EndGame : MonoBehaviour
         {
             if (collision.gameObject == agent[i] && rbAgent[i].velocity.y == 0)
             {
-                endText.text = "You lose";
-                EndingScreenEnable();
+                LoadScene(3);
                 return; // Exit early once we find a matching agent
             }
         }
 
         if (collision.gameObject == player && rbPlayer.velocity.y == 0)
         {
-            endText.text = "You win";
-            EndingScreenEnable();
+            LoadScene(2);
         }
     }
 
-    private void EndingScreenEnable()
+    private void LoadScene(int id)
     {
-        end = true;
-        EndingScreen.SetActive(true);
-        Vector3 playerPosition = player.transform.position;
-        EndingScreen.transform.position = new Vector3(playerPosition.x, playerPosition.y, playerPosition.z + 5f);
-        EndSound.SetActive(true);
+        SceneManager.LoadScene(id);
     }
 }
